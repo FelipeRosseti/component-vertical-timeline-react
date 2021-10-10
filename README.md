e611c594d18daa71a9f5e040a94391760fd76e0a
Felipe Rosseti <felipe@orbitdigital.com.br>
2021-10-10 07:16:43 -0300

    add package files

---
+++ README.md
@@ -0,0 +1,16 @@
+# component-vertical-timeline-to-react
+
+React component for simple vertical timeline creation
+
+
+## Getting started
+
+To install as npm dependency
+
+```sh
+npm install --save component-vertical-timeline-to-react
+```
+
+## License
+
+MIT
---
+++ src/config/index.js
@@ -0,0 +1,110 @@
+export const defaultValues = {
+	theme: {
+		yearColor: '#888888',
+		lineColor: '#c5c5c5',
+		dotColor: '#c5c5c5',
+		borderDotColor: '#ffffff',
+		titleColor: '#cccccc',
+		subtitleColor: '#888888',
+		textColor: '#cccccc',
+	},
+	lang: 'pt',
+	dateFormat: 'L',
+};
+
+export const monthArray = {
+	pt: [
+		'Jan',
+		'Fav',
+		'Mar',
+		'Abr',
+		'Mai',
+		'Jun',
+		'Jul',
+		'Ago',
+		'Set',
+		'Out',
+		'Nov',
+		'Dez',
+	],
+	es: [
+		'Ene',
+		'Feb',
+		'Mar',
+		'Abr',
+		'May',
+		'Jun',
+		'Jul',
+		'Ago',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dic',
+	],
+	en: [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	],
+	de: [
+		'Jan.',
+		'Feb.',
+		'März',
+		'Apr.',
+		'Mai.',
+		'Juni',
+		'Juli',
+		'Aug.',
+		'Sep.',
+		'Okt.',
+		'Nov.',
+		'Dez.',
+	],
+	tr: [
+		'Ocak',
+		'Şubat',
+		'Mart',
+		'Nisan',
+		'Mayıs',
+		'Haziran',
+		'Temmuz',
+		'Ağustos',
+		'Eylül',
+		'Ekim',
+		'Kasım',
+		'Aralık'
+	]
+};
+
+export const mapText = {
+	pt: {
+		from: 'De',
+		to: 'Até',
+	},
+	en: {
+		from: 'From',
+		to: 'To',
+	},
+	es: {
+		from: 'Desde',
+		to: 'Hasta',
+	},
+	de: {
+		from: 'Von',
+		to: 'Bis',
+	},
+	tr: {
+		from: 'Başlangıç',
+		to: 'Bitiş'
+		
+	}
+};
